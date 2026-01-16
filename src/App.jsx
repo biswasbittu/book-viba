543222820d013248c8a62cce2039339c671770d1
Bittu Biswas <biswasbittu7415@yahoo.com>
2026-01-16 17:57:20 +0530

    tailwind daisy ui and google font done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-center text-2xl font-bold'>Book Viba</h1>
+    </div>
+  );
+};
+
+export default App;
